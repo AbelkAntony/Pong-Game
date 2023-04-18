6a85198c586c4c02987718f87c183c1a6de90387
ABEL K ANTONY <abelkantony98@gmail.com>
2023-04-18 20:48:21 +0530

    Update EnemyPaddle.cs

--- Assets/Scripts/EnemyPaddle.cs
+++ Assets/Scripts/EnemyPaddle.cs
@@ -7,6 +7,27 @@ public class EnemyPaddle : Paddle
     public Rigidbody2D ball;
     private void FixedUpdate()
     {
-        
+        if (this.ball.velocity.x > 0.0f)
+        {
+            if(this.ball.position.y > this.transform.position.y)
+            {
+                _rigidbody.AddForce(Vector2.up * this.speed);
+            }
+            else if(this.ball.position.y < this.transform.position.y)
+            {
+                _rigidbody.AddForce(Vector2.down * this.speed);
+            }
+        }
+        else
+        {
+            if(this.transform.position.y > 0.0f)
+            {
+                _rigidbody.AddForce(Vector2.down * this.speed);
+            }
+            else if (this.transform.position.y < 0.0f)
+            {
+                _rigidbody.AddForce(Vector2.up * this.speed);
+            }
+        }
     }
 }
